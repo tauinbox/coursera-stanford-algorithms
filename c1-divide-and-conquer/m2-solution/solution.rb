ca63f4cb720308ca4803f85d7223ce7b48d9a9d9
Alexander Tupavov <tauinbox@gmail.com>
2016-10-14 07:54:50 +0300

    revised, added more descriptive comments

--- c1-divide-and-conquer/m2-solution/solution.rb
+++ c1-divide-and-conquer/m2-solution/solution.rb
@@ -1,5 +1,5 @@
 @a = []
-@inversion = 0
+@inversions = 0
 
 def load_data(filename)
   if File.exist? filename
@@ -17,32 +17,25 @@ def sort_and_count(a)
   n = a.length
   return a if n == 1
 
+  # divide and conquer!
   middle = (n.to_f/2).ceil
-
 
   left_half = a[0...middle]
   right_half = a[(middle)...n]
 
-  # puts "______________________________________________________________"
-  # puts("left: #{left_half}")
-  # puts("right: #{right_half}")
-  # puts "______________________________________________________________"
-
-  b = []
-  c = []
-  d = []
-
   b = sort_and_count(left_half)
   c = sort_and_count(right_half)
+  d = merge_and_count_inversions(b, c, n)
 
-  # puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> b is: #{b}"
-  # puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> c is: #{c}"
+  return d
+
+end
+
+def merge_and_count_inversions(b, c, n)
 
   out = []
-  i = 0
-  j = 0
-
-  b_empty, c_empty = false
+  i = j = 0
+  b_empty = c_empty = false
 
   n.times do
     # puts
@@ -58,9 +51,11 @@ def sort_and_count(a)
     # puts "c.length: #{c.length}"
     # puts
 
+    # check if one of two parts (left and right) is empty
     if !b_empty && !c_empty
 
       if (b[i] <= c[j])
+        # no inversion, just copy the smallest to output
         out << b[i]
         # puts "=== b < c === put #{b[i]} to out, out is #{out}"
         i += 1
@@ -69,11 +64,13 @@ def sort_and_count(a)
           # puts "b is empty"
         end
       else
+        # we found inversion! means b[j] > c[j], take the smallest to output
         out << c[j]
         # puts "=== c < b === inversion detected! put #{c[j]} to out, out is #{out}"
         
-        # check for all split inversions
-        @inversion += (b.length - i)
+        # check for all split inversions. means that all residual elements after 'i' in left part 'b' are split inversions
+        # because 'b' is already sorted, so just count those inversions
+        @inversions += (b.length - i)
 
         j += 1
         if (c.length - j) == 0
@@ -104,10 +101,10 @@ def sort_and_count(a)
 
   # puts("++++++++++++++++++++ out: #{out} ++++++++++++++++++++")
 
-  return out
+  return out  
 
 end
 
 # puts "++++++++++++++++++++ #{sort_and_count(@a)} +++++++++++++++++"
 sort_and_count(@a)
-puts @inversion
+puts @inversions
