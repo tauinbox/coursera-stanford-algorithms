0f1e064ca6383390e775bfe5d5028445815b28e7
Alexander Tupavov <tauinbox@gmail.com>
2016-10-26 09:08:19 +0300

    revised. OOP implementation

--- c1-divide-and-conquer/m2-solution/solution.rb
+++ c1-divide-and-conquer/m2-solution/solution.rb
@@ -3,118 +3,128 @@
 
 ###########################################################################################################################
 
-def load_data(filename)
+class MergeSort
 
-  a ||= []
+  attr_reader :inversions, :result
 
-  if File.exist? filename
-    File.foreach (filename) do |line|
-      a << line.chomp.to_i
-    end
+  def initialize(filedata)
+    a = load_data(filedata)
+    @result = sort_and_count(a)
   end
 
-  return a
+  def load_data(filename)
 
-end
+    a ||= []
 
-###########################################################################################################################
+    if File.exist? filename
+      File.foreach (filename) do |line|
+        a << line.chomp.to_i
+      end
+    end
 
-def sort_and_count(a)
+    return a
 
-  n = a.length
-  return a if n == 1
+  end
 
-  # divide and conquer!
-  middle = (n.to_f/2).ceil
+  ###########################################################################################################################
 
-  left_half = a[0...middle]
-  right_half = a[(middle)...n]
+  def sort_and_count(a)
 
-  # puts
-  # puts "left_half is #{left_half}, right_half is #{right_half}"
+    n = a.length
+    return a if n == 1
 
-  b = sort_and_count(left_half)
-  c = sort_and_count(right_half)
-  d = merge_and_count_inversions(b, c, n)
+    # divide and conquer!
+    middle = (n.to_f/2).ceil
 
-  return d
+    left_half = a[0...middle]
+    right_half = a[(middle)...n]
 
-end
-
-###########################################################################################################################
-
-def merge_and_count_inversions(b, c, n)
-
-  #initialize an inversions counter
-  @inversions ||= 0
-
-  out = []
-  i = j = 0
-  b_empty = c_empty = false
-
-  n.times do
-    # puts "i = #{i}, j = #{j}, n = #{n}"
     # puts
+    # puts "left_half is #{left_half}, right_half is #{right_half}"
 
-    # puts "b[#{i}] = #{b[i]}, c[#{j}] = #{c[j]}"
-    # puts
+    b = sort_and_count(left_half)
+    c = sort_and_count(right_half)
+    d = merge_and_count_inversions(b, c, n)
 
-    # puts "b.length: #{b.length}"
-    # puts "c.length: #{c.length}"
-    # puts
+    return d
 
-    # check if one of two parts (left and right) is empty
-    if !b_empty && !c_empty
+  end
 
-      if (b[i] <= c[j])
-        # no inversion, just copy the smallest to output
-        out << b[i]
-        # puts "=== b < c === put #{b[i]} to out, out is #{out}"
-        i += 1
-        if (b.length - i) == 0
-          b_empty = true
-          # puts "b is empty"
+  ###########################################################################################################################
+
+  def merge_and_count_inversions(b, c, n)
+
+    #initialize an inversions counter
+    @inversions ||= 0
+
+    out = []
+    i = j = 0
+    b_empty = c_empty = false
+
+    n.times do
+      # puts "i = #{i}, j = #{j}, n = #{n}"
+      # puts
+
+      # puts "b[#{i}] = #{b[i]}, c[#{j}] = #{c[j]}"
+      # puts
+
+      # puts "b.length: #{b.length}"
+      # puts "c.length: #{c.length}"
+      # puts
+
+      # check if one of two parts (left and right) is empty
+      if !b_empty && !c_empty
+
+        if (b[i] <= c[j])
+          # no inversion, just copy the smallest to output
+          out << b[i]
+          # puts "=== b < c === put #{b[i]} to out, out is #{out}"
+          i += 1
+          if (b.length - i) == 0
+            b_empty = true
+            # puts "b is empty"
+          end
+        else
+          # we found inversion! means b[i] > c[j], take the smallest to output
+          out << c[j]
+          # puts "=== c < b === inversion detected! put #{c[j]} to out, out is #{out}"
+          
+          # check for all split inversions. means that all residual elements after 'i' in left part 'b' are split inversions
+          # because 'b' is already sorted, so just count those inversions
+          @inversions += (b.length - i)
+
+          j += 1
+          if (c.length - j) == 0
+            c_empty = true
+            # puts "c is empty"
+          end
         end
-      else
-        # we found inversion! means b[i] > c[j], take the smallest to output
-        out << c[j]
-        # puts "=== c < b === inversion detected! put #{c[j]} to out, out is #{out}"
-        
-        # check for all split inversions. means that all residual elements after 'i' in left part 'b' are split inversions
-        # because 'b' is already sorted, so just count those inversions
-        @inversions += (b.length - i)
 
+      elsif b_empty
+        out << c[j]
+        # puts "b is already empty, geting #{c[j]} from c, out is #{out}"
         j += 1
         if (c.length - j) == 0
           c_empty = true
           # puts "c is empty"
         end
+      elsif c_empty
+        out << b[i]
+        # puts "c is already empty, getting #{b[i]} from b, out is #{out}"      
+        i += 1
+        if (b.length - i) == 0
+          b_empty = true
+          # puts "b is empty"
+        end
       end
 
-    elsif b_empty
-      out << c[j]
-      # puts "b is already empty, geting #{c[j]} from c, out is #{out}"
-      j += 1
-      if (c.length - j) == 0
-        c_empty = true
-        # puts "c is empty"
-      end
-    elsif c_empty
-      out << b[i]
-      # puts "c is already empty, getting #{b[i]} from b, out is #{out}"      
-      i += 1
-      if (b.length - i) == 0
-        b_empty = true
-        # puts "b is empty"
-      end
     end
 
+    # puts("++++++++++++++++++++ out: #{out} ++++++++++++++++++++")
+
+    return out  
+
   end
-
-  # puts("++++++++++++++++++++ out: #{out} ++++++++++++++++++++")
-
-  return out  
-
 end
 
 ###########################################################################################################################
@@ -122,11 +132,10 @@ end
 input_file = 'IntegerArray.txt'
 # input_file = 'TestArray.txt'
 
-a = load_data input_file
+sorted_array = MergeSort.new(input_file)
 
-sorted_array = sort_and_count(a)
-# puts sorted_array
+# puts sorted_array.result
 
 puts "\n-----------------------------------------------------------"
-puts "Found #{@inversions} inversions in input array #{input_file}"
+puts "Found #{sorted_array.inversions} inversions in input array #{input_file}"
 puts "-----------------------------------------------------------\n"
