b121943e2817d81312d88d87ff2cb37d5544f242
Alexander Tupavov <tauinbox@gmail.com>
2016-11-15 10:17:32 +0300

    add more comments

--- c2-graph-search/m2-solution/solution.rb
+++ c2-graph-search/m2-solution/solution.rb
@@ -6,15 +6,21 @@
 
 class Dijkstra
 
-  attr_reader :distances
+  attr_reader :a, :b, :x, :ten_distances
 
   def initialize(filedata)
 
     # init graph by loading data from file
     @graph = {}
     load_data filedata
+
+    # explored verices data with boolean frontier attribute
     @x = {}
+
+    # distances data from start point to all other vertices
     @a = {}
+
+    # paths data from start point to all other vertices
     @b = {}
 
     # puts @graph.inspect
@@ -25,20 +31,17 @@ class Dijkstra
     @x[start_vertex] = true
     @a[start_vertex] = 0
     @b[start_vertex] = [start_vertex]
-    # puts @x
 
+    # run searching procedure
     search_path
 
-    puts "final result. @a = #{@a.inspect}"
+    @ten_distances = []
+    [7,37,59,82,99,115,133,165,188,197].each do |node|
+      @ten_distances << @a[node]
+      puts "\nShortest path to #{node}: #{@b[node].inspect}, length = #{@a[node]}"
+    end
 
-    # @distances = []
-    # [7,37,59,82,99,115,133,165,188,197].each do |node|
-    #   @distances << @a[node]
-    #   puts "\npath: #{@b[node].inspect}, length = #{@a[node]}"
-    # end
-
-    # puts
-    # puts @distances.join(',')
+    @ten_distances = @ten_distances.join(',')
 
   end
 
@@ -55,11 +58,12 @@ class Dijkstra
 
   def search_path
     while @x.length < @graph.length do
-      puts "\n============================================================"
-      puts "[@x.length = #{@x.length}, @graph.length = #{@graph.length}]\n\r"
+      # puts "\n============================================================"
+      # puts "[@x.length = #{@x.length}, @graph.length = #{@graph.length}]\n\r"
       greedy_choose_one
-      gets
-    end 
+      # gets
+    end
+    # another one (last) execution when @x.length is equal to @graph.length
     greedy_choose_one
   end
 
@@ -68,63 +72,70 @@ class Dijkstra
     frontier = []
 
     @x.each do |key, value|
+      # check for frontier boolean attribute
       if value 
         frontier << key
       end
     end
 
-    puts "frontier: #{frontier}"
+    # puts "frontier: #{frontier}"
 
     selector = {}
     link = {}
 
     frontier.each do |node|
-      puts "looking for the nearest neighbour for #{node}..."
+      # puts "looking for the nearest neighbour for #{node}..."
 
       # find nearest for each frontier node
       nearest = get_nearest_neighbour(node)
 
-      # combine all nearests in one selector
-
-      # nearest[1] - distance
-      # nearest[0] - found vertex
-
       if nearest
-        selector[node] = nearest[1] + @a[node]
+        # combine all calculated distances in one selector
+        # nearest[1] - distance to found vertex
+        # nearest[0] - found vertex
+        # @a[node] - already calculated distance to node      
 
+        # Dijkstra's greedy criterion
+        selector[node] = @a[node] + nearest[1]
+
+        # link between tail and head (edge)
         link[node] = nearest[0]
 
-        puts "\nfound one: #{nearest}, overall: #{selector.inspect}"
-        puts "edge: #{node} - #{nearest[0]}, length = #{@a[node]} + #{nearest[1]} = #{@a[node] + nearest[1]}\n\r"
+        # puts "\nfound one: #{nearest}, overall: #{selector.inspect}"
+        # puts "edge: #{node} - #{nearest[0]}, length = #{@a[node]} + #{nearest[1]} = #{@a[node] + nearest[1]}\n\r"
       else
-        puts "Can't find any outter neighbours for #{node}"
+        # puts "Can't find any outter neighbours for #{node}"
       end
     end
 
-    # greedy search result
+    # greedy search result (take one with minimum distance to starting point 's')
     result = selector.min_by{|k, v| v}
 
     if result
-      puts "result is #{result}, let's add it to @x"
-      puts "draw the route from: #{result[0]} to #{link[result[0]]} with length #{result[1]}"
+      # puts "result is #{result}, let's add it to @x"
+      # puts "draw the route from: #{result[0]} to #{link[result[0]]} with length #{result[1]}"
 
       # add result to @x
       @x[link[result[0]]] = true
 
+      # push result distance
       @a[link[result[0]]] = result[1]
+
+      # push result path
       @b[link[result[0]]] = @b[result[0]] + [link[result[0]]]
 
-      puts "\nadded result for #{link[result[0]]}, a = #{@a[link[result[0]]]}\n\r"
-      puts "\npath for #{link[result[0]]}, @b = #{@b[link[result[0]]]}\n\r"
+      # puts "\nadded result for #{link[result[0]]}, a = #{@a[link[result[0]]]}\n\r"
+      # puts "\npath for #{link[result[0]]}, @b = #{@b[link[result[0]]]}\n\r"
 
+      # update frontier attribute
       frontier.each do |node|
         if all_neighbours_in_x?(node)
-          puts "all neighbours for #{node} are already in @x"
+          # puts "all neighbours for #{node} are already in @x"
           @x[node] = false
         end
       end
 
-      puts "\n@x now is: #{@x.inspect}"
+      # puts "\n@x now is: #{@x.inspect}"
 
     end
 
@@ -161,11 +172,13 @@ end
 
 ###########################################################################################################################
 
-# input_file = 'dijkstraData.txt'
-input_file = 'testArray.txt'
+input_file = 'dijkstraData.txt'
+# input_file = 'testArray.txt'
 
 solution = Dijkstra.new(input_file)
 
-# puts "\n----------------------------------------------------------------------"
-# puts "\nDistances to the given ten vertices: #{solution.distances}"
-# puts "----------------------------------------------------------------------\n"
+# puts "\nShortest paths data:\nA(s) = #{solution.a}"
+
+puts "\n-------------------------------------------------"
+puts "Distances to the given ten vertices:\n#{solution.ten_distances}"
+puts "-------------------------------------------------\n"
