dacb2492551b87749d1ef40f31a14e20ffba4f4f
Alexander Tupavov <tauinbox@gmail.com>
2016-12-08 10:30:13 +0300

    working solution

--- c3-greedy-algorithms/m1-solution/solution-3.rb
+++ c3-greedy-algorithms/m1-solution/solution-3.rb
@@ -10,19 +10,29 @@ class Prims
 
   def initialize(filedata)
 
-    # init graph by loading data from file
+    # init undirected graph by loading data from file
+    # graph structure example:
+    # {
+    #  node1 => {node2 => cost, node3 => cost},
+    #  node2 => {node1 => cost, node5 => cost}, ...
+    # }
     @graph = {}
     load_data filedata
-    puts @graph.inspect
-    puts @graph.length
+    # puts @graph.inspect
+    # puts @graph.length
 
     # explored verices data with boolean frontier attribute
+    # {node1 => frontier_attribute1, node2 => frontier_attribute2, ...}
     @x = {}
+    @t = {}
+    @cost = 0
 
-    # put first vertex into X
+    # put the vertex number 1 into X
     start_vertex = 1
     # set the frontier attrubute to true
-    @x[start_vertex] = true    
+    @x[start_vertex] = true
+
+    search_mst
 
   end
 
@@ -82,41 +92,36 @@ class Prims
         # combine all calculated distances in one selector
         # nearest[1] - distance to found vertex
         # nearest[0] - found vertex
-        # @a[node] - already calculated distance to node      
 
-        # 
-        selector[node] = @a[node] + nearest[1]
+        # hash of nearest distances from the frontier list to outer space
+        # set found nearest distance from current node in loop to outer vertex
+        selector[node] = nearest[1]
 
-        # link between tail and head (edge)
+        # remember the link between tail and head (edge)
         link[node] = nearest[0]
 
         # puts "\nfound one: #{nearest}, overall: #{selector.inspect}"
-        # puts "edge: #{node} - #{nearest[0]}, length = #{@a[node]} + #{nearest[1]} = #{@a[node] + nearest[1]}\n\r"
       else
         # puts "Can't find any outter neighbours for #{node}"
       end
     end
 
-    # greedy search result (take one with minimum distance to starting point 's')
+    # greedy search result (take one with minimum distance to verticises in frontier list)
     result = selector.min_by{|k, v| v}
 
     if result
       # puts "result is #{result}, let's add it to @x"
-      # puts "draw the route from: #{result[0]} to #{link[result[0]]} with length #{result[1]}"
 
-      # add result to @x
+      # add result to @x and set it up the frontier atttribute
+      # link[result[0]] here will get us the vertex to which we found the result
       @x[link[result[0]]] = true
 
-      # push result distance
-      @a[link[result[0]]] = result[1]
+      # augment the cost value with found result
+      @cost += result[1]
 
-      # push result path
-      @b[link[result[0]]] = @b[result[0]] + [link[result[0]]]
+      # puts "Now cost is: #{@cost}"
 
-      # puts "\nadded result for #{link[result[0]]}, a = #{@a[link[result[0]]]}\n\r"
-      # puts "\npath for #{link[result[0]]}, @b = #{@b[link[result[0]]]}\n\r"
-
-      # update frontier attribute
+      # update frontier attribute, set it down for all of those, whose neighbours are already in @x
       frontier.each do |node|
         if all_neighbours_in_x?(node)
           # puts "all neighbours for #{node} are already in @x"
@@ -138,6 +143,7 @@ class Prims
       hash.delete(key) if hash.key?(key)
     end
 
+    # return node with minimum cost
     hash.min_by {|k, v| v}
   end
 
@@ -166,6 +172,6 @@ input_file = 'edges.txt'
 
 solution = Prims.new(input_file)
 
-puts "\n-------------------------------------------------"
-puts "The overall cost of a minimum spanning tree:\n#{solution.cost}"
-puts "-------------------------------------------------\n"
+puts "\n--------------------------------------------------------------------"
+puts "3. Prim's MST. The overall cost of a minimum spanning tree: #{solution.cost}"
+puts "--------------------------------------------------------------------\n"
