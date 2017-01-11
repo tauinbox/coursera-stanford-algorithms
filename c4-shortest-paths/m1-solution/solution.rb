6af3d8a53e43efdce5791e10a3cbf0a04c5751b3
Alexander Tupavov <tauinbox@gmail.com>
2017-01-11 18:16:02 +0300

    alpha implementation

--- c4-shortest-paths/m1-solution/solution.rb
+++ c4-shortest-paths/m1-solution/solution.rb
@@ -16,11 +16,14 @@ class APSP
     #  node1 => {node2 => cost, node3 => cost},
     #  node2 => {node1 => cost, node5 => cost}, ...
     # }
-    
+
     @graph = {}
+    @a = {}
     load_data filedata
-    # puts @graph.inspect
-    puts @graph.length
+    puts @graph.inspect
+    # puts @graph.length
+
+    find_from_source(1)
 
   end
 
@@ -35,11 +38,71 @@ class APSP
           next
         else
           # create subhash if not exists
-          @graph[line[0]] = Hash.new if !@graph[line[0]]
+          @graph[line[0]] = Hash.new(nil) if !@graph[line[0]]
+          @graph[line[1]] = Hash.new(nil) if !@graph[line[1]]
           # populate it with cost values
-          @graph[line[0]][line[1]] = line[2]
+          @graph[line[1]][line[0]] = line[2]
         end
       end
+    end
+  end
+
+  def find_from_source(s)
+    @a[0] = {}
+    @a[0][s] = 0
+
+    for i in 1..@num_vertices - 1
+      puts "\ni = #{i}"
+      @graph.each do |vertex|
+        puts "\n----------------------------------------------\nLOOKING AT VERTEX #{vertex}"
+        case1 = @a[i - 1][vertex.first]
+        case2 = get_minimum_inbound_path(i, vertex.first)
+        puts "case1: #{case1}, case2: #{case2}"
+        @a[i] = Hash.new if !@a[i]
+        @a[i][vertex.first] = case2 ? (case1 ? [case1, case2].min : case2) : case1
+        puts "set A[#{i}][#{vertex.first}] to #{@a[i][vertex.first]}"
+        puts "A: #{@a.inspect}"
+        gets        
+      end
+    end
+  end
+
+  def get_minimum_inbound_path(i, vertex)
+    if @graph[vertex].length > 0
+      sample_edge = @graph[vertex].first
+      # puts "sample_edge: #{sample_edge}"
+
+      path_to_tail = @a[i - 1][sample_edge[0]]
+      # puts "path_to_tail: #{path_to_tail}"
+
+      if path_to_tail
+
+        min_path = path_to_tail + sample_edge[1]
+        # puts "let's set sample_edge to #{sample_edge}, min_path to #{min_path}"
+
+        @graph[vertex].each do |inbound|
+          puts "checking vertex #{inbound}"
+
+          path_to_tail = @a[i - 1][inbound[0]]
+          puts "path_to_tail (A[#{i - 1}][#{inbound[0]}]): #{path_to_tail}"
+
+          if path_to_tail
+            path = path_to_tail + inbound[1]
+
+            if path < min_path
+              puts "[path scanning] found smaller one: #{path}"
+              min_path = path
+            end          
+          end
+        end
+        puts "The smallest path for vertex #{vertex} is #{min_path}"
+        return min_path
+      else
+        return false
+      end
+    else
+      puts "There is no one inbound arch to vertex #{vertex}"
+      return false
     end
   end
 
@@ -47,8 +110,8 @@ end
 
 ###########################################################################################################################
 
-input_file = 'g1.txt'
-# input_file = 'testArray.txt'
+# input_file = 'g1.txt'
+input_file = 'testArray.txt'
 
 solution = APSP.new(input_file)
 
