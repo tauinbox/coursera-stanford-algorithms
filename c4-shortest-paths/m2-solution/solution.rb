2d32553e4e63de5bb408f034aded1f9720c1a013
Alexander Tupavov <tauinbox@gmail.com>
2017-01-17 19:09:44 +0300

    alpha version

--- c4-shortest-paths/m2-solution/solution.rb
+++ c4-shortest-paths/m2-solution/solution.rb
@@ -6,7 +6,7 @@
 
 class TSP
 
-  attr_reader :num_edges, :num_vertices
+  attr_reader :s_length
 
   def initialize(filedata)
 
@@ -19,13 +19,75 @@ class TSP
     # }
 
     @graph = {}
+    @a = {}
     @coordinates = []
-    load_data filedata
+    # load_data filedata
+
+    @graph = {
+      1=>{2=>2, 4=>3, 3=>1},
+      2=>{1=>2, 3=>4, 4=>5},
+      3=>{1=>1, 2=>4, 4=>6},
+      4=>{3=>6, 1=>3, 2=>5}
+    }
+
+    # puts @s_length
 
     puts "Coordinates length: #{@coordinates.length}"
     puts "\nCoordinates: #{@coordinates.inspect}"
     puts "\nGraph: #{@graph.inspect}"
 
+
+    # set the basecase ('s' has only one vertex)
+    @a[1] = {}
+    @a[1][1] = 0
+
+    for m in 2..@graph.length
+      for s in 1..m
+        puts "\n--------------------------------------------------------\nConsider vertex #{s} from set of #{m} vertices"
+
+        # iterate through relevant choices of j in current set s
+        for j in 1..s
+
+          # skip the start vertex (vertex 1 in this case)
+          if j == 1
+            puts "- Skip vertex 1 (it's starting point and can't be j)"
+            next
+          end
+
+          puts "\nLets j = #{j}\n\n"
+
+          @a[s] = Hash.new if !@a[s]
+          @a[s][j] = choose_min(s, j)
+
+          puts "\nSet A[#{s}][#{j}] = #{@a[s][j]}\nA = #{@a.inspect}"
+          gets
+        end
+      end
+    end
+
+  end
+
+  def choose_min(s, j)
+    puts "Calculating A[#{s}][#{j}]..."
+    array_to_choose_min = []
+
+    for k in 1..s
+      puts "\nLooking at vertex k = #{k} from possible set of #{s}"
+
+      # skip if 'k' is equal to 'j'
+      if k == j
+        puts "Excluded! (#{k}), because it's j-vertex\n\n"
+        next
+      end
+
+      puts "A[#{k}][#{k}] = #{@a[k][k]}, Edge length to 'j' = #{@graph[k][j]}"
+      array_to_choose_min << @a[k][k] + @graph[k][j] if @a[k][k]
+      # array_to_choose_min << (@a[s - k][k] ? @a[s - k][k] + @graph[k][j] : nil)
+      puts "array: #{array_to_choose_min.inspect}"
+    end
+
+    puts "array_to_choose_min: #{array_to_choose_min.inspect}"
+    return array_to_choose_min.min
   end
 
   # Method for loading data from file
@@ -34,8 +96,7 @@ class TSP
       File.foreach (filename) do |line|
         line = line.chomp.split(" ").map(&:to_f)
         if line.length == 1
-          # @num_vertices = line[0]
-          # @num_edges = line[1]
+          @s_length = line[0].to_i
           next
         else
           @coordinates << line
