72c7f64a18152f474e8a8976d9d7310d7b28443e
Alexander Tupavov <tauinbox@gmail.com>
2017-01-19 10:18:21 +0300

    refactored, added some debugging info

--- c4-shortest-paths/m2-solution/solution.rb
+++ c4-shortest-paths/m2-solution/solution.rb
@@ -21,9 +21,9 @@ class TSP
     @graph = {}
     @a = {}
     @coordinates = []
+    all_subsets = []
 
-    # @all_subsets = []
-    # load_data filedata
+    load_data filedata
 
     @graph = {
       1=>{2=>2, 4=>3, 3=>1},
@@ -34,59 +34,74 @@ class TSP
 
     # puts @s_length
 
-    puts "Coordinates length: #{@coordinates.length}"
+    puts "\nCoordinates number: #{@coordinates.length}"
     puts "\nCoordinates: #{@coordinates.inspect}"
-    puts "\nGraph: #{@graph.inspect}"
+    puts "\nGraph: #{@graph.inspect}\n\n"
 
 
     # set the basecase for all sets for vertex 1
 
+    # for i in 2..@graph.length + 1
+    #   for subset in all_subsets_with_size(i - 2)
+    #     set = [1] + subset
+
+    #     @a[set] = Hash.new if !@a[set]
+
+    #     if set == [1]
+    #       @a[set][1] = 0
+    #     else
+    #       @a[set][1] = Float::INFINITY
+    #     end        
+
+    #   end
+    # end
+
+    first_hop = [1]
+    @a[first_hop] = {}
+    @a[first_hop][1] = 0
+
     for i in 2..@graph.length + 1
       for subset in all_subsets_with_size(i - 2)
-        set = [1] + subset
-
-        # @all_subsets << set
-
-        @a[set] = Hash.new if !@a[set]
-
-        if set == [1]
-          @a[set][1] = 0
-        else
-          @a[set][1] = Float::INFINITY
-        end        
-
+        all_subsets << [1] + subset
       end
     end
 
-    # puts "All subsets: #{@all_subsets}"
+    puts "Generating's done!\n\n"
 
-    puts "\nA: #{@a}"
+    # puts "\nAll subsets: #{all_subsets.inspect}"
+
+    # puts "\nA: #{@a}"
 
     for m in 2..@graph.length
 
-      for subset in all_subsets_with_size(m - 1)
+      puts "Passing through subsets with length #{m}..."
 
-        set = [1] + subset
+      for subset in all_subsets
 
-        # puts "Set: #{set.inspect}"
+        next if subset.length < m
+        break if subset.length > m
 
-        for j in set
+        for j in subset
           if j != 1
-            subset_without_j = set - [j]
+            subset_without_j = subset - [j]
 
-            puts "\n------- Lets j = #{j}, Set: #{set}, Subset without j: #{subset_without_j.inspect}\n\n"
+            # puts "\n------- Lets j = #{j}, Subset: #{subset}, Subset without j: #{subset_without_j.inspect}\n\n"
 
             array_to_choose_min = []
 
-            for k in set
+            for k in subset
               if k != j
-                puts "\nConsider k = #{k}, A#{subset_without_j}[#{k}] = #{@a[subset_without_j][k]}, distance from k to j = #{@graph[k][j]}"
-                array_to_choose_min << @a[subset_without_j][k] + @graph[k][j]
+                @a[subset_without_j] = Hash.new if !@a[subset_without_j]
+                if @a[subset_without_j][k]
+                  # puts "\nConsider k = #{k}, A#{subset_without_j}[#{k}] = #{@a[subset_without_j][k]}, distance from k to j = #{@graph[k][j]}"
+                  array_to_choose_min << @a[subset_without_j][k] + @graph[k][j]
+                end
               end
             end
-            puts "\nArray to choose minimum: #{array_to_choose_min.inspect}"
-            @a[set][j] = array_to_choose_min.min
-            puts "Chosen minimum: #{@a[set][j]}"
+            # puts "\nArray to choose minimum: #{array_to_choose_min.inspect}"
+            @a[subset] = Hash.new if !@a[subset]
+            @a[subset][j] = array_to_choose_min.min
+            # puts "Chosen minimum: #{@a[subset][j]}"
           end
         end
 
@@ -110,6 +125,7 @@ class TSP
   end
 
   def all_subsets_with_size(size)
+    puts "Generating subsets with size #{size}"
     set = (2..@graph.length).to_a
     set.combination(size).to_a
   end
